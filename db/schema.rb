3ffdaedee7e334929dc66e64260ca2817acea071
Jim O'Kelly (rubycasts.io) <thatrubylove@gmail.com>
2015-05-22 05:51:32 -0500

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150519083922) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
